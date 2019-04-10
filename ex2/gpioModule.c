79c44d36fa3c71efcc069db5df989dba02bb08bd
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2019-04-10 18:28:16 +0900

    led on.off

--- ex2/gpioModule.c
+++ ex2/gpioModule.c
@@ -21,15 +21,19 @@
 #include <linux/fs.h> //struct file_operation
 #include <linux/cdev.h> //charactor device
 #include <linux/io.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/gpio.h> //gpio_get_value()
 
 #define GPIO_MAJOR 200
 #define GPIO_MINOR 0
 #define GPIO_DEVICE "gpioled"
 #define GPIO_LED 18
+#define BLK_SIZE 100
+
+/*
 //라즈베리파이 GPIO  물리메모리
 #define BCM_IO_BASE 0x3F000000
 
@@ -49,28 +53,80 @@
 
 #define GPIO_GET(g) (*(gpio+13)&(1<<g))
 #define GPIO_SIZE 0xB4
-
+*/
 
 //#define DEBUG
 
 struct cdev gpio_cdev;
-volatile unsigned int *gpio;
+//volatile unsigned int *gpio;
+static char msg[BLK_SIZE]={0};
 
 
+static int gpio_open(struct inode *, struct file *); //file_operation 과 함수이름 맞춰줘야한다!!
+static int gpio_close(struct inode *, struct file *);
+static ssize_t gpio_read(struct file *, char *buff, size_t , loff_t *); //user 단으로 데이터 전송
+static ssize_t gpio_write(struct file *, const char *, size_t , loff_t *);
 
 static struct file_operations gpio_fops={
 	.owner   = THIS_MODULE,
-//	.read    = gpio_read,
-//	.write   = gpio_write,
-//	.open    = gpio_open,
-//	.release = gpio_close,
+	.read    = gpio_read,
+	.write   = gpio_write,
+	.open    = gpio_open,
+	.release = gpio_close,
 };
+
+//						inode주소 			file 정보 주소 를 인자로
+static int gpio_open(struct inode *inod, struct file *fil) //file_operation 과 함수이름 맞춰줘야한다!!
+{
+	//모듈의 사용 카운트를 증가시킨다
+	try_module_get(THIS_MODULE);//try_module_get은 현재 이장치를 사용하고 있는 프로세서의 갯수를 알려준다 
+								//프로세서가 장치를 사용하고 있을때는 rmmod가 불가능하다 따라서 open시 이것을 채크
+	printk(KERN_INFO "GPIO Device opened\n");
+	return 0;
+}
+
+static int gpio_close(struct inode *inod, struct file *fil)
+{
+	//모듈의 사용 카운트를 감소시킨다.
+	module_put(THIS_MODULE); // <-> try_module_get()
+	printk(KERN_INFO "GPIO Device closed\n");
+	return 0;
+}
+						//파일의 위치	어떤버퍼를 읽어올지 , 읽어올 사이즈 ,버퍼의 오프셋값
+static ssize_t gpio_read(struct file *fil, char *buff, size_t len, loff_t *off) //user 단으로 데이터 전송
+{
+	int count;
+	if(gpio_get_value(GPIO_LED)) //gpio_get_value = 현재 gpio 의 값을 읽어옴
+			msg[0]='1';
+	else
+			msg[1]='0';
+	strcat(msg," from kernel"); // print msg + from kernel
+	count = copy_to_user(buff,msg,strlen(msg)+1); //유저단에 msg 보내줌 +1 은  널문자
+	
+	printk(KERN_INFO "GPIO Device read: %s\n",msg);
+
+	return count;
+	
+}
+
+static ssize_t gpio_write(struct file *fil, char const *buff, size_t len, loff_t *off)
+{
+	int count;
+	memset(msg, 0, BLK_SIZE);
+
+	count = copy_from_user(msg,buff,len); //유저단의 buff가져옴
+	gpio_set_value(GPIO_LED, (strcmp(msg, "0"))); //gpio 에 값을 써줌
+
+	printk(KERN_INFO "GPIO Device write: %s\n", msg);
+
+	return count;
+}
 
 static int __init initModule(void)
 {
 	dev_t devno; //dev_t 디바이스 파일의 MAJOR , MINOR 번호를 지정하기 위한 변수타입(32bit)
 	int err;
-	static void *map;
+	//static void *map;
 	int count;
 
 	printk("called initModule()\n");
@@ -78,7 +134,11 @@ static int __init initModule(void)
 	// 1. 문자디바이스 드라이버를 등록한다
 	devno = MKDEV(GPIO_MAJOR, GPIO_MINOR); //MAJOR , MINOR 값을 이용하여 dev_t값을 얻는다
 	register_chrdev_region(devno,1,GPIO_DEVICE); //dev_t 값을 집어 넣어서 디바이스 드라이버 등록
-/*	
+
+
+
+/*
+
 	register_chrdev_region() :  문자디바이스를 등록한다
 	
 	함수의 동작과정
@@ -97,6 +157,9 @@ static int __init initModule(void)
 	=> 커널에서 char_device_struct를 공개하지 않고 cdev만 공개 , 사용하도록하기 위함
 
 */
+
+
+
 	//2. 문자 디바이스를 위한 구조체를 초기화 한다
 	cdev_init(&gpio_cdev, &gpio_fops); //디바이스를 초기화 하고 , 사용자가 생성한 fops를 생성한 cdev의 struct file_operation을 등록
 	gpio_cdev.owner = THIS_MODULE;
@@ -112,7 +175,15 @@ static int __init initModule(void)
 	}
 	printk(KERN_INFO "'sudo mknod /dev/%s c %d 0'\n",GPIO_DEVICE, GPIO_MAJOR);
 	printk(KERN_INFO "'sudo chmod 666 /dev/%s'\n",GPIO_DEVICE);
-
+	//현재 GPIO_LED핀이 사용중인지 확인하고 사용권한 획득
+	err= gpio_request(GPIO_LED, "LED"); 
+	
+	if(err==-EBUSY)
+	{
+		printk(KERN_INFO "Error gpio_request\n");
+		return -1;
+	}
+	/* =
 	//4. 물리메모리 번지로 인자값을 전달하면 가상메모리 번지를 리턴 (ioremap)
 	map = ioremap(GPIO_BASE, GPIO_SIZE);
 
@@ -122,34 +193,44 @@ static int __init initModule(void)
 		iounmap(map);
 		return -EBUSY;
 	}
-
+	
 #ifdef DEBUG
 	printk(KERN_INFO "devno=%d",devno);
 #endif
 
 	gpio = (volatile unsigned int*)map;
+	*/
 
-	GPIO_OUT(GPIO_LED);
-	//GPIO_CLR(GPIO_LED); //프로그램이 시작될때 led를 꺼놓고 시작  
-	GPIO_SET(GPIO_LED); //test
-
+	gpio_direction_output(GPIO_LED,0); //gpio select function= output ,pin =GPIOLED (18), gpio_value=0
+										//chipset 회사에서 이러한 함수를 지원해줌으로서 우리가 직접적으로 register를 건들이지 않아도 된다
+/* = GPIO_OUT(GPIO_LED);
+	 //GPIO_CLR(GPIO_LED); //프로그램이 시작될때 led를 꺼놓고 시작  
+	 GPIO_SET(GPIO_LED); //test
+*/
 	return 0;
 }
 
 static void __exit cleanupModule(void)
 {
 	dev_t devno = MKDEV(GPIO_MAJOR,GPIO_MINOR);
-	GPIO_CLR(GPIO_LED);
+	
+	gpio_direction_output(GPIO_LED,0);
+	//=GPIO_CLR(GPIO_LED);
+
+	//gpio_request()에서 받아온 사용권한을 반납한다.
+	gpio_free(GPIO_LED);
+	
 	// 1. 문자디바이스 등록 해제
 	unregister_chrdev_region(devno,1);
 
 	// 2. 문자디바이스 구조체를 제거
 	cdev_del(&gpio_cdev);
-
+	
 	//할당된 자원을 반납
-	if(gpio) //if(gpio) == 사용한 gpio 번지가 있다면 1이기때문에 반납하게된다
+	/*if(gpio) //if(gpio) == 사용한 gpio 번지가 있다면 1이기때문에 반납하게된다
 			iounmap(gpio);
-
+	*/
+	
 	printk("Good-bye!\n");
 }
 
